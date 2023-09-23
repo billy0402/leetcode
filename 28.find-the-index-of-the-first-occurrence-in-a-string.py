ac4f216e3f25f9c25da30752cf8ece9f4e1e0c1b
Billy Huang <10646003@ntub.edu.tw>
2023-09-23 23:40:13 +0800

    feat(python3): 28.find-the-index-of-the-first-occurrence-in-a-string

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=28 lang=python3
+#
+# [28] Find the Index of the First Occurrence in a String
+#
+
+
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        try:
+            return haystack.index(needle)
+        except ValueError:
+            return -1
+
+
+# @lc code=end
