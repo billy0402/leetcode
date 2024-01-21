25ba8f48d9c0c7f2366b091c5140c06fcecba21b
Billy Huang <10646003@ntub.edu.tw>
2024-01-21 15:27:20 +0800

    fix(python3): 136.single-number - use xor

--- 136.single-number.py
+++ 136.single-number.py
@@ -4,19 +4,14 @@
 # [136] Single Number
 #
 
+
 # @lc code=start
-from typing import List
-
-
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        nums.sort()
-
-        for i in range(0, len(nums) - 1, 2):
-            if nums[i] != nums[i + 1]:
-                return nums[i]
-
-        return nums[-1]
+    def singleNumber(self, nums: list[int]) -> int:
+        xor = 0
+        for n in nums:
+            xor ^= n
+        return xor
 
 
 # @lc code=end
