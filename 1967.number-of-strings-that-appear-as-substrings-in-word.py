fc2589575a2be6e2565549d00d2eae77d50bdac3
Billy Huang <10646003@ntub.edu.tw>
2024-02-02 18:50:52 +0800

    feat(python3): 1967.number-of-strings-that-appear-as-substrings-in-word

---
+++ 1967.number-of-strings-that-appear-as-substrings-in-word.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1967 lang=python3
+#
+# [1967] Number of Strings That Appear as Substrings in Word
+#
+
+
+# @lc code=start
+class Solution:
+    def numOfStrings(self, patterns: list[str], word: str) -> int:
+        return sum(pattern in word for pattern in patterns)
+
+
+# @lc code=end
