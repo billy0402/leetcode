860ef08e8b64c4c430c4f8c993ec48e4b9d0a2f7
Billy Huang <10646003@ntub.edu.tw>
2024-03-10 11:23:21 +0800

    fix(python3): 349.intersection-of-two-arrays - replace built in set intersection with manual method

--- 349.intersection-of-two-arrays.py
+++ 349.intersection-of-two-arrays.py
@@ -8,13 +8,16 @@
 # @lc code=start
 class Solution:
     def intersection(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        def set_intersection(set1: set, set2: set):
+            return list(n for n in set1 if n in set2)
+
         set1 = set(nums1)
         set2 = set(nums2)
 
-        if len(set2) < len(set1):
-            return list(set2.intersection(set1))
+        if len(set2) > len(set1):
+            return set_intersection(set2, set1)
 
-        return list(set1.intersection(set2))
+        return set_intersection(set1, set2)
 
 
 # @lc code=end
