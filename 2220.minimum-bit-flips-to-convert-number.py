91586bd731001a561cdee6d5500784f75fbd99ee
Billy Huang <10646003@ntub.edu.tw>
2024-09-11 10:40:26 +0800

    feat(python3): 2220.minimum-bit-flips-to-convert-number

---
+++ 2220.minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=2220 lang=python3
+#
+# [2220] Minimum Bit Flips to Convert Number
+#
+
+# @lc code=start
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        return (start ^ goal).bit_count()
+
+
+# @lc code=end
