8bf078d10fdcf438f4b9eb49aab4f84b90dfc144
Billy Huang <10646003@ntub.edu.tw>
2025-01-01 22:32:28 +0800

    feat(python3): 1422.maximum-score-after-splitting-a-string - use one pass

--- 1422.maximum-score-after-splitting-a-string.py
+++ 1422.maximum-score-after-splitting-a-string.py
@@ -8,17 +8,21 @@
 # @lc code=start
 class Solution:
     def maxScore(self, s: str) -> int:
-        result = 0
+        score: int = float("-inf")  # pyright:ignore[reportAssignmentType]
+        zeros = 0
+        ones = 0
+
         for i in range(len(s) - 1):
-            total = 0
-            for j in range(i + 1):
-                if s[j] == '0':
-                    total += 1
-            for j in range(i + 1, len(s)):
-                if s[j] == '1':
-                    total += 1
-            result = max(result, total)
-        return result
+            if s[i] == "0":
+                zeros += 1
+            elif s[i] == "1":
+                ones += 1
+            score = max(score, zeros - ones)  # left use ones
+
+        if s[-1] == "1":
+            ones += 1
+
+        return score + ones
 
 
 # @lc code=end
