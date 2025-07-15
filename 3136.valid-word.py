3626245507f41caf7694c4050b011ec694de17fc
Billy Huang <10646003@ntub.edu.tw>
2025-07-15 16:31:46 +0800

    feat(python3): 3136.valid-word

---
+++ 3136.valid-word.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=3136 lang=python3
+#
+# [3136] Valid Word
+#
+
+# @lc code=start
+MIN_LENGTH = 3
+VOWELS = {"a", "e", "i", "o", "u"}
+
+
+class Solution:
+    def isValid(self, word: str) -> bool:
+        if len(word) < MIN_LENGTH:
+            return False
+
+        has_vowel = False
+        has_consonant = False
+        for c in word:
+            if c.isalpha():
+                if c.lower() in VOWELS:
+                    has_vowel = True
+                else:
+                    has_consonant = True
+            elif not c.isdigit():
+                return False
+        return has_vowel and has_consonant
+
+
+# @lc code=end
